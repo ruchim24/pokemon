06911301b0ca7b89795c0e4ae2b262657fc4a6c0
ruchim24 <ruchimajoka24@gmail.com>
2024-06-17 01:06:59 +0530

    improved css and added spinner during api response loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Pokemon from './Pokemon';
+import { useState } from 'react';
 
 function App() {
+  const [isLoading,setIsLoading] = useState(false);
   return (
-    <div className="App">
-     <Pokemon />
+    <div className='App'>
+     <Pokemon isLoading={isLoading} setIsLoading={setIsLoading}/>
     </div>
   );
 }
--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,129 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-function Pokemon() {
+function Pokemon({isLoading,setIsLoading}) {
     const [next, setNext] = useState('');
     const [prev, setPrev] = useState('');
     const [data, setData] = useState([]);
-    const [imgs,setImgs] = useState([]);
+    const [page,setPage] = useState(0);
+    const [prevData,setPrevData] = useState([]);
+    const [imgLoad,setImgLoad] = useState(true);
+
+    const initialRender = useRef(false);
 
 
-    const fetchData = async () => {
+    const fetchData = async (api) => {
         try {
-            const response = await fetch(' https://pokeapi.co/api/v2/pokemon?limit=3&offset=0');
+            setIsLoading(true);
+            const response = await fetch(api);
             const res = await response.json();
             setNext(res.next);
             setPrev(res.previous);
-            //setData([...res.results]);
+        
             let arr = [];
-            arr = await res?.results?.map( async (d) => {
+            arr = res?.results?.map( async (d) => {
                 const url = await fetch(d.url);
-                const img = await url.json();
-                // setData([...data,{name:d.name,url:img?.sprites?.front_default}])
-                // arr.push(img?.sprites?.front_default)
-                return ({name:d.name,url:img?.sprites?.front_default})
+                const pokedata = await url.json();
+              
+                return ({name:pokedata?.name,url:pokedata?.sprites?.front_default,id:pokedata?.id,pageNo:page})
             })
+            console.log(arr,"oo");
             const ans = await Promise.all(arr);
             setData(ans);
+            setPrevData([...prevData,ans]);
+            setPage(page+1);
         } catch (err) {
             console.log(err);
         }
+        finally{
+          setImgLoad(true);
+           setIsLoading(false);
+        }
     }
 
-
     useEffect(() => {
-        fetchData();
+        if(initialRender.current){
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=3&offset=0';
+        fetchData(url);
+        }
+        initialRender.current = true;
     }, []);
 
-    // useEffect(() => {
-    //     const arr = [];
-    //     if(data.length>0){
-    //         data?.map(async (d) => {
-    //             // fetch(d.url)
-    //             // .then((res) => res.json())
-    //             // .then((info) => {
-    //             //     arr.concat(info?.sprites?.front_default);
-    //             //     console.log(info?.sprites?.front_default);
-    //             //     setImgs([...imgs,info?.sprites?.front_default]);
-    //             // });
-    //             const res = await fetch(d.url);
-    //             const Data = await res.json();
-    //             console.log(Data?.sprites?.front_default);
-    //         })
-    //         console.log(arr,"arr")
-    //     }
-    // },[data]);
+   
 
-
-    const handleNext = async () => {
-        try {
-            const response = await fetch(next);
-            // const res = await response.json();
-            // setNext(res.next);
-            // setPrev(res.previous);
-            // setData([...res.results]);
-            const res = await response.json();
-            setNext(res.next);
-            setPrev(res.previous);
-            //setData([...res.results]);
-            let arr = [];
-            arr = await res?.results?.map( async (d) => {
-                const url = await fetch(d.url);
-                const img = await url.json();
-                // setData([...data,{name:d.name,url:img?.sprites?.front_default}])
-                // arr.push(img?.sprites?.front_default)
-                return ({name:d.name,url:img?.sprites?.front_default})
-            })
-            const ans = await Promise.all(arr);
-            setData(ans);
-
-        } catch (err) {
-
-        }
+    const handleNext = () => {
+        fetchData(next);
     };
 
-    const handlePrev = async() => {
-        try {
-            if(prev){
-            const response = await fetch(prev);
-            // const res = await response.json();
-            // setNext(res.next);
-            // setPrev(res.previous);
-            // setData([...res.results]);
-            const res = await response.json();
-            setNext(res.next);
-            setPrev(res.previous);
-            //setData([...res.results]);
-            let arr = [];
-            arr = await res?.results?.map( async (d) => {
-                const url = await fetch(d.url);
-                const img = await url.json();
-                // setData([...data,{name:d.name,url:img?.sprites?.front_default}])
-                // arr.push(img?.sprites?.front_default)
-                return ({name:d.name,url:img?.sprites?.front_default})
-            })
-            const ans = await Promise.all(arr);
-            setData(ans);
-            }
-
-        } catch (err) {
-
-        }
+    const handlePrev = () => {
+        fetchData(prev);
     }
 
     return (
         <>
-            <div>Pokemon</div>
-            <div className='row'>
+            {isLoading && 
+            <div className='backdrop'>
+            <div className='loader'></div>
+            </div>
+            }
+            <div className='cards-box'>
+                <div className='cards-main'>
                 {data?.map((d, index) => {
                     return (
-                        <div key={d?.name} className='col-md-4'>
-                            <p>{d?.name}</p>
-                            <img src={d?.url} />
+                        <div key={d?.name} className='cards'>
+                            <div className='cards-content'>
+                                <span>{d?.name}</span>
+                                <span>ID: {d?.id}</span>
+                            </div>
+                            <div className='cards-img'>
+                                { imgLoad && <p className='imgloading'>Loading...</p>}
+                                <img src={d?.url} loading='lazy' aria-placeholder={d.name} alt={d?.name} onLoad={() => setImgLoad(false)}/>
+                            </div>
                         </div>
                     )
                 })}
+                </div>
+                <div className='cards-btn'>
                 <button onClick={() => handlePrev()} disabled={prev ? false : true}>Prev</button>
                 <button onClick={() => handleNext()}>Next</button>
+                </div>
             </div>
         </>
     );
